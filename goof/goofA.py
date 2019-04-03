2ea519cec756e593b1d24de00fd427ddd17be890
Abhijit Suprem <asf2182@columbia.edu>
2019-04-03 10:49:10 -0400

    moved away unneded code

--- goof/goofA.py
+++
@@ -1,8 +0,0 @@
-import pdb
-from goofB import *
-import goofB as goofC
-
-print hello
-print bye
-
-pdb.set_trace()
--- goof/goofB.py
+++
@@ -1,5 +0,0 @@
-hello = "hello"
-bye = __name__
-
-if __name__ == "__main__":
-    hello = "goodbye"
