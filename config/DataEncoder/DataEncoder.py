e32ef813044f958d73648423bd425e5b39a3c27d
Abhijit Suprem <asf2182@columbia.edu>
2019-04-03 10:58:14 -0400

    added abstract classes

---
+++ config/DataEncoder/DataEncoder.py
@@ -0,0 +1,17 @@
+
+
+
+class DataEncoder():
+    """ Super class for Data Encoders. They must implement methods presented here """
+
+    def __init__(self,):
+        """ Initialize Encoder """
+        pass
+
+    def encode(self, data):
+        """ Encode given data into some format """
+        
+        pass
+        encodedData = data
+        return encodedData
+
