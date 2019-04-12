ff115a7b154e0f7ed4c586c0df9e685dcb806b8e
Rodrigo A. Lima <rodrigoalima99@gmail.com>
2019-04-12 15:52:31 -0400

    Add unit test and comment to time_to_id.

--- utils.py
+++ utils.py
@@ -26,10 +26,15 @@ def ms_to_readable(ms, format="%Y-%m-%d %H:%M:%S"):
     ms -- [int] Timestamp in milliseconds to be formatted.
     format -- [str] Format string.
     """
-    return datetime.datetime.fromtimestamp(ms / 1000).strftime(format)
+    return datetime.datetime.fromtimestamp(ms/1000).strftime(format)
 
-def time_to_id(ms=None, lval = 5):
-    DICTA={str(idx):item for idx,item in enumerate("abcdefghij")}
+def time_to_id(ms=None, lval=5):
+    """Return a string obtained from mapping digits of the specified timestamp into characters.
+
+    ms -- [int] Timestamp in milliseconds to be transformed into a string. If it is None, use
+    current timestamp.
+    """
+    DICTA = {str(idx): item for (idx, item) in enumerate("abcdefghij")}
     if ms is None:
         ms = time.time()
     ms_str = ("%."+str(lval)+"f")%ms
--- utils_test.py
+++ utils_test.py
@@ -9,6 +9,7 @@ class TestUtils(unittest.TestCase):
     def test_std_flush(self):
         """Redirect standard output to a temporary buffer, call std_flush, and test the content
         written to that temporary buffer."""
+        ori_stdout = sys.stdout
         red_stdout = io.StringIO()
         sys.stdout = red_stdout
         # No parameters.
@@ -17,6 +18,8 @@ class TestUtils(unittest.TestCase):
         # Parameters of different types.
         utils.std_flush(42, "Hello", [1, 2, 3])
         self.assertEqual(red_stdout.getvalue(), "\n42 Hello [1, 2, 3]\n")
+        # Reset original stdout.
+        sys.stdout = ori_stdout
 
     def test_readable_time(self):
         # Test standard format.
@@ -37,6 +40,12 @@ class TestUtils(unittest.TestCase):
             datetime.datetime.strptime(utils.readable_time("%H:%M:%S"), "%H:%M:%S").second in
             [datetime.datetime.now().second, (datetime.datetime.now().second - 1) % 60]
         )
+
+    def test_time_to_id(self):
+        """Test if returned string is non-empty and contains appropriate alphabetic characters."""
+        test_id = utils.time_to_id()
+        self.assertTrue(len(test_id) > 0)
+        self.assertEqual(len(test_id), len([c for c in test_id if c in "abcdefghij"]))
 
 if __name__ == "__main__":
     unittest.main()
