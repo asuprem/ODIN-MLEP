8a8ba937aa5bab0d12e1e08a2d91c1667e922988
Abhijit Suprem <asf2182@columbia.edu>
2019-04-17 17:09:13 -0400

    moved and separted learning models. changed to learning_model

---
+++ mlep/mlep/learning_model/sklearnDecisionTree.py
@@ -0,0 +1,17 @@
+
+from LearningModel import LearningModel
+
+
+class sklearnDecisionTree(LearningModel):
+    """Decision Tree learning model wrapper."""
+
+    def __init__(self):
+        """Initialize a Decison Tree learning model."""
+        from sklearn.tree import DecisionTreeClassifier
+        super(sklearnDecisionTree,self).__init__(DecisionTreeClassifier(max_depth=10))
+
+    
+    def isUpdatable(self):
+        """ Decision Tree is not Updatable """
+        return False
+    
---
+++ mlep/mlep/learning_model/sklearnLogReg.py
@@ -0,0 +1,20 @@
+
+from LearningModel import LearningModel
+
+
+class sklearnLogReg(LearningModel):
+    """Logistic regression learning model wrapper."""
+
+    def __init__(self):
+        """Initialize a logistic regression learning model wrapper."""
+        from sklearn.linear_model import LogisticRegression
+        super(sklearnLogReg,self).__init__(LogisticRegression(solver="lbfgs", max_iter=100000, warm_start=True))
+
+    def update(self, X, y):
+        """Update the Logistic Regression model to the training data.
+        X -- [array of shape (n_samples, n_features)] Training data.
+        y -- [array of shape (n_samples)] Target values for the training data.
+        """
+        self.fit(X, y)
+
+    
