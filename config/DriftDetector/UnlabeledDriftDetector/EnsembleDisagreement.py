b7fc0ce553e3b17f301d8cec0181dc098f813c5e
Abhijit Suprem <asf2182@columbia.edu>
2019-04-17 16:14:47 -0400

    fixed bug in ensembledisagreement

--- config/DriftDetector/UnlabeledDriftDetector/EnsembleDisagreement.py
+++ config/DriftDetector/UnlabeledDriftDetector/EnsembleDisagreement.py
@@ -2,10 +2,11 @@ import UnlabeledDriftDetector
 
 
 class EnsembleDisagreement(UnlabeledDriftDetector.UnlabeledDriftDetector):
-    def __init__(self,threshold=0.8):
+    def __init__(self,threshold=0.8, min_ensemble=5):
 
         from itertools import combinations
         self.threshold = threshold
+        self.min_ensemble = min_ensemble
         self.reset()
         self.combinations = combinations
 
@@ -24,12 +25,12 @@ class EnsembleDisagreement(UnlabeledDriftDetector.UnlabeledDriftDetector):
         returns -- is there drift
         """
         #required for DDM
-        import pdb
-        pdb.set_trace()   
+        if len(ensembleClassification) < self.min_ensemble:
+            return False
         comp = [item for item in self.combinations(ensembleClassification, 2)] 
         delta = [0]*len(comp)
         delta_sum = 0.0
-        idx_
+        idx_ = 0
         for idx,scoreTuple in enumerate(comp):
             delta[idx] = float(scoreTuple[0] == scoreTuple[1])
             delta_sum+=delta[idx]
