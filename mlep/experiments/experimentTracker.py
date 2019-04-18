23e870f4a55d0f9be5695ff2318fd74164668b16
Abhijit Suprem <asf2182@columbia.edu>
2019-04-18 01:51:23 -0400

    set up automated explicit drift analysis runs

--- mlep/experiments/experimentTracker.py
+++ mlep/experiments/experimentTracker.py
@@ -1,20 +1,33 @@
 import os, time, json, sys, pdb, click
-import utils
 
-import MLEPServer
-from config.DataModel import BatchedLocal
-from config.DataModel import StreamLocal
-from config.DataSet import PseudoJsonTweets
+import mlep.core.MLEPServer as MLEPServer
+
+import mlep.data_model.BatchedLocal as BatchedLocal
+import mlep.data_model.StreamLocal as StreamLocal
+import mlep.data_set.PseudoJsonTweets as PseudoJsonTweets
+import mlep.utils.io_utils as io_utils
+import mlep.utils.time_utils as time_utils
+
 
 import mlflow
 
 from sklearn.model_selection import ParameterGrid
 
+import traceback
+
+LOG_FILE = "./logfiles/application.log"
+EXP_STATUS = "./logfiles/experiments.log"
 def main():
+
+    exp_status_write = open(EXP_STATUS, "a")
+    exp_status_write.write("\n\n\n\n")
+    exp_status_write.write("--------------------------------------")
+    exp_status_write.write("  BEGINNING NEW EXECUTION AT " + str(time_utils.readable_time("%Y-%m-%d %H:%M:%S"))) 
+    exp_status_write.write("  --------------------------------------"+ "\n\n")
     # We are tracking drift adaptivity
     # namely labeled drift detection, unlabeled-ensembleagreement versus some scheduled runs for now
     # So we are running three experiments
-
+    
     # set up scheduled params:
     scheduled_param_grid = {  "update": [("2592000000", "M"), ( "1210000000","F")], 
                     "weights":[( "unweighted","U"),( "performance","P" )], 
@@ -22,13 +35,20 @@ def main():
                     "filter":[("no-filter","F") , ("top-k","T"),("nearest","N")],
                     "kval":[("5","5")],
                     "allow_update_schedule": [True]}
-    pdb.set_trace()
     scheduled_param = ParameterGrid(scheduled_param_grid)
 
     # Set up explicit drift detection params
-    explicit_drift_param_grid = {   "allow_explicit_drift": [True],
-                                    "explicit_drift_class": ["LabeledDriftDetector"],
-                                    "explicit_drift_mode":["DDM", "EDDM", "ADWIN", "PageHinkley"]}
+    explicit_drift_param_grid = {   "allow_explicit_drift": [(True,"ExpDr")],
+                                    "explicit_drift_class": [("LabeledDriftDetector","LDD")],
+                                    "explicit_drift_mode":[("DDM","DDM"), ("EDDM","EDDM"), ("ADWIN","ADWIN"), ("PageHinkley", "PageHinkley")], 
+
+                                    "allow_unlabeled_drift": [(False,"")],
+                                    "allow_update_schedule": [(False,"")],
+
+                                    "weights":[( "unweighted","U"),( "performance","P" )],
+                                    "select":[(  "recent","RR" ) , ( "recent-new","RN" ) , ( "recent-updates","RU" ) , ( "historical-new","HN" ) , ( "historical-updates","HU" ) , ( "historical","HH" )],
+                                    "filter":[("no-filter","F") , ("top-k","T"),("nearest","N")],
+                                    "kval":[("5","5"), ("10","10")]}
     explicit_drift_params = ParameterGrid(explicit_drift_param_grid)
 
     # Set up unlabeled drift detection params
@@ -39,54 +59,79 @@ def main():
     # Set up parameters:
     
 
-    for param_set in unlabeled_drift_param:
+    for param_set in explicit_drift_params:
         # This is an experiment
         
         # Load up configuration file
-        mlepConfig = utils.load_json('./config/configuration/MLEPServer.json')
+        mlepConfig = io_utils.load_json('./MLEPServer.json')
 
-        # Update config file
+        # Update config file and generate an experiment name
+        experiment_name=''
         for _param in param_set:
-            mlepConfig[_param] = param_set[_param]
+            if param_set[_param][1] != "":
+                experiment_name+=param_set[_param][1] + '-'
+            mlepConfig["config"][_param] = param_set[_param][0]
+        experiment_name = experiment_name[:-1]
     
-    # We'll load thhe config file, make changes, and write a secondary file for experiments
-    CONFIG_PATH = './config/configuration/ExperimentalConfig.json'
-    with open(CONFIG_PATH, 'w') as write_:
-        write_.write(json.dumps(mlepConfig))
+        # We'll load thhe config file, make changes, and write a secondary file for experiments
+        CONFIG_PATH = './ExperimentalConfig.json'
+        with open(CONFIG_PATH, 'w') as write_:
+            write_.write(json.dumps(mlepConfig))
 
-    # Now we have the Experimental Coonfig we can use for running an experiment
-    runExperiment(CONFIG_PATH)
+        # Now we have the Experimental Coonfig we can use for running an experiment
+        # generate an experiment name
+        exp_status_write.write("--STATUS-- " + experiment_name + "   ")
+        try:
+            runExperiment(CONFIG_PATH, mlepConfig, experiment_name)
+            exp_status_write.write("SUCCESS\n")
+        except Exception as e:
+            exp_status_write.write("FAILED\n")
+            exp_status_write.write(traceback.format_exc())
+            exp_status_write.write(str(e))
+            exp_status_write.write("\n")
+
+    exp_status_write.close()
 
 
 
 
-def runExperiment(PATH_TO_CONFIG_FILE):
+def runExperiment(PATH_TO_CONFIG_FILE, mlepConfig, experiment_name):
 
     # set up mlflow access
-
     # mlflow.set_tracking_uri -- not needed, defaults to mlruns
-
     # mlflow.create_experiment -- need experiment name. Should I programmatically create one? or go by timestamp
+    sys.stdout = open(LOG_FILE, "w")
 
-    mlflow.start_run()
+    #mlflow.set_tracking_uri("mysql://mlflow:mlflow@127.0.0.1:3306/mlflow_runs")
+    mlflow.start_run(run_name="explicit_drift_analysis")
 
-    # log_param
-    # Need to log current git id -- add this as a tag
-    # Need to log entire configuration..
+    # Log relevant details
+    for _key in mlepConfig["config"]:
+        # possible error
+        if _key != "drift_metrics":
+            mlflow.log_param(_key, mlepConfig["config"][_key])
+    mlflow.log_param("experiment_name", experiment_name)
+
 
     internalTimer = 0
-    streamData = StreamLocal.StreamLocal(data_source="data/data/2014_to_dec2018.json", data_mode="single", data_set_class=PseudoJsonTweets.PseudoJsonTweets)
+    streamData = StreamLocal.StreamLocal(data_source="data/2014_to_dec2018.json", data_mode="single", data_set_class=PseudoJsonTweets.PseudoJsonTweets)
 
-    augmentation = BatchedLocal.BatchedLocal(data_source='data/data/collectedIrrelevant.json', data_mode="single", data_set_class=PseudoJsonTweets.PseudoJsonTweets)
+    augmentation = BatchedLocal.BatchedLocal(data_source='data/collectedIrrelevant.json', data_mode="single", data_set_class=PseudoJsonTweets.PseudoJsonTweets)
     augmentation.load_by_class()
 
-    trainingData = BatchedLocal.BatchedLocal(data_source='data/data/initialTrainingData.json', data_mode="single", data_set_class=PseudoJsonTweets.PseudoJsonTweets)
+    trainingData = BatchedLocal.BatchedLocal(data_source='data/initialTrainingData.json', data_mode="single", data_set_class=PseudoJsonTweets.PseudoJsonTweets)
     trainingData.load()
 
     # Now we have the data
     MLEPLearner = MLEPServer.MLEPLearningServer(PATH_TO_CONFIG_FILE)
 
-    totalCounter = []
+    # Perform initial traininig
+    MLEPLearner.initialTrain(traindata=trainingData)
+    io_utils.std_flush("Completed training at", time_utils.readable_time())
+    MLEPLearner.addAugmentation(augmentation)
+    io_utils.std_flush("Added augmentation at", time_utils.readable_time())
+
+    totalCounter = 0.0
     mistakes = []
     while streamData.next():
         if internalTimer < streamData.getObject().getValue("timestamp"):
@@ -96,18 +141,28 @@ def runExperiment(PATH_TO_CONFIG_FILE):
         classification = MLEPLearner.classify(streamData.getObject())
 
 
-        totalCounter.append(1)
+        totalCounter += 1.0
         if classification != streamData.getLabel():
             mistakes.append(1.0)
         else:
             mistakes.append(0.0)
-        if len(totalCounter) % 1000 == 0 and len(totalCounter)>0:
-            utils.std_flush("Completed", len(totalCounter), " samples, with running error (past 100) of", sum(mistakes[-100:])/sum(totalCounter[-100:]))
-        if len(totalCounter) % 100 == 0 and len(totalCounter)>0:
-            savePath.write(str(sum(mistakes[-100:])/sum(totalCounter[-100:]))+',')
+        if totalCounter % 1000 == 0 and totalCounter>0.0:
+            io_utils.std_flush("Completed", int(totalCounter), " samples, with running error (past 100) of", sum(mistakes[-100:])/100.0)
+        if totalCounter % 100 == 0 and totalCounter>0.0:
+            running_error = sum(mistakes[-100:])/100.0
+            mlflow.log_metric("running_err"+str(int(totalCounter/100)), running_error)
     
-    mlflow.end_run()    
 
+    MLEPLearner.shutdown()
+
+    io_utils.std_flush("\n-----------------------------\nCOMPLETED\n-----------------------------\n")
+    
+    sys.stdout.close()
+    
+    mlflow.log_param("total_samples", totalCounter)  
+    mlflow.log_artifact(LOG_FILE)
+    mlflow.log_param("run_complete", True)
+    mlflow.end_run()
 
 
 
