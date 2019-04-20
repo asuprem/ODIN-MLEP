79df3b75ccc0ccc8373f76ac31abac3d4d65b108
Abhijit Suprem <asf2182@columbia.edu>
2019-04-20 06:13:11 -0400

    updated base learner with new functions

--- mlep/mlep/learning_model/BaseLearningModel.py
+++ mlep/mlep/learning_model/BaseLearningModel.py
@@ -71,7 +71,7 @@ class BaseLearningModel:
             X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=1.0-split, random_state = 42, shuffle=True, stratify=y_train)
     
         self.update(X_train, y_train)
-        precision, recall, score = self.precision_recall_fscore(X_test, y_test)
+        precision, recall, score = self._precision_recall_fscore(X_test, y_test)
         return precision, recall, score
 
     def fit_and_test(self, X_train, y_train, split = 0.7, X_test = None, y_test = None):
@@ -98,15 +98,51 @@ class BaseLearningModel:
             X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=1.0-split, random_state = 42, shuffle=True, stratify=y_train)
     
         self.fit(X_train, y_train)
-        precision, recall, score = self.precision_recall_fscore(X_test, y_test)
+        precision, recall, score = self._precision_recall_fscore(X_test, y_test)
         return precision, recall, score
 
-    def predict(self, X_sample):
+    def _precision_recall_fscore(self, X, y):
+        """Return a 3-tuple where the first element is the precision of the model, the second is the
+        recall, and the third is the F-measure.
+        For statistical learning models, the test data is represented by an array of dimension
+        (n_samples, n_features);
+        
+        X -- [array] Test data.
+        y -- [array] Target values for the test data.
+        """
+        from sklearn.metrics import precision_recall_fscore_support
+        y_pred = self.predict(X, mode="test")
+        prs_ = tuple(precision_recall_fscore_support(y, y_pred, average="weighted")[0:3])
+        return prs_[0], prs_[1], prs_[2]
+
+    def predict(self, X_sample, mode = "predict", y_label = None):
         """Return predicted labels for the test data.
-        X -- [array of shape (n_samples, n_features)] Test data.
+
+        Given a data point, predict will perform one of three things based on the value of mode:
+            - predict/test -- Return the model's prediction of the label of a given sample X_sample
+            - implicit --   Perform prediction and return the mode's prediction of label. In addition, store a history of the model's confidence and set internal drift detector
+                            Then, calling model's driftDetected() method next time should return True if drift was detected
+            
+            - explicit --   Perform prediction. Detect drift based on predicted value, confidence, and actual value. Update internal drift detector
+
+        
+        Args:
+            X_sample: [array of shape (n_samples, n_features)] Test data.
+            mode:   "predict" -- for standard prediction. This includes no drift tracking
+                    "implicit" -- for implicit drift tracking. This is for unlabeled examples
+                    "explicit" -- for explicit drift tracking. This is for labeled examples
+                    "test" -- internal-use. No drift tracking is performed. Used during testing/evaluating model post training or update.
         """
         
-        prediction = self._predict(X_sample = X_sample)
+        
+        if mode == "predict" or mode == "test":
+            prediction = self._predict(X_sample = X_sample)
+        elif mode == "implicit":
+            prediction = self._evaluate_implicit_drift(X_sample)
+        elif mode == "explicit":
+            prediction = self._evaluate_explicit_drift(X_sample, y_label)
+        else:
+            raise NotImplementedError()
         return prediction
 
 
@@ -121,20 +157,16 @@ class BaseLearningModel:
         except ValueError:
             return self._model.predict(X_sample.reshape(1,-1))
 
-
-    def precision_recall_fscore(self, X, y):
-        """Return a 3-tuple where the first element is the precision of the model, the second is the
-        recall, and the third is the F-measure.
-        For statistical learning models, the test data is represented by an array of dimension
-        (n_samples, n_features);
+    def _evaluate_implicit_drift(self,X_sample):
+        """ This evaluates a model's implicit drift """
         
-        X -- [array] Test data.
-        y -- [array] Target values for the test data.
-        """
-        from sklearn.metrics import precision_recall_fscore_support
-        y_pred = self.predict(X)
-        prs_ = tuple(precision_recall_fscore_support(y, y_pred, average="weighted")[0:3])
-        return prs_[0], prs_[1], prs_[2]
+        raise NotImplementedError()
+
+    def _evaluate_explicit_drift(self,X_sample, y_label):
+        """ This evaluates a model's explicit drift using y_label """
+        
+        raise NotImplementedError()
+
 
     def clone(self, LearningModelToClone):
         """Clone the LearningModelToClone into this model. They must be the same model Type
@@ -168,7 +200,6 @@ class BaseLearningModel:
         if _track is not None:
             self.track_drift = _track
         return self.track_drift
-
 
             
     
