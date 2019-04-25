4af10a2b1ee381ea07e7297667a183ffb8b1b6c2
Abhijit Suprem <asf2182@columbia.edu>
2019-04-25 18:48:46 -0400

    Added PseudoJsonReddit to PseudoJsonTweetsFile

--- mlep/mlep/data_set/PseudoJsonTweets.py
+++ mlep/mlep/data_set/PseudoJsonTweets.py
@@ -8,4 +8,11 @@ class PseudoJsonTweets(mlep.data_set.PseudoJson.PseudoJson):
         super(PseudoJsonTweets,self).__init__(data, dataKey="text", labelKey="label")
 
 
+class PseudoJsonReddit(mlep.data_set.PseudoJson.PseudoJson):
+    """ PseudoJsonReddit model parses a psuedojson line and extracts relevant details from it """
+
+    def __init__(self,data):
+        super(PseudoJsonReddit,self).__init__(data, dataKey="title", labelKey="label")
+
+
     
