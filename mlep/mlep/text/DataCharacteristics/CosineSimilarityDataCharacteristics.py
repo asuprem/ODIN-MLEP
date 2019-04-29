d94d29034bd7b16a40ddf359926f1c364edd282e
Abhijit Suprem <asf2182@columbia.edu>
2019-04-29 02:30:52 -0400

    Update data characteristics to use metrics

--- mlep/mlep/text/DataCharacteristics/CosineSimilarityDataCharacteristics.py
+++ mlep/mlep/text/DataCharacteristics/CosineSimilarityDataCharacteristics.py
@@ -1,10 +1,12 @@
-import mlep.text.DataCharacteristics.OnlineSimilarityDistribution as OnlineSimilarityDistribution
-import mlep.metrics.TextMetrics as TextMetrics
+import mlep.tools.distributions.CosineSimilarityDistribution as CosineSimilarityDistribution
+import mlep.tools.metrics.TextMetrics as TextMetrics
+import mlep.utils.array_utils as array_utils
 class CosineSimilarityDataCharacteristics:
     def __init__(self, nBins=40, alpha = 0.6):
         self.nBins = nBins
-        self.distribution = OnlineSimilarityDistribution.OnlineSimilarityDistribution(nBins)
+        self.distribution = None
         self.alpha = alpha
+
         self.attrs={}
         self.attrs["centroid"] = None
         self.attrs["delta_low"] = None
@@ -19,45 +21,20 @@ class CosineSimilarityDataCharacteristics:
         """
 
         self.attrs["centroid"] = centroid
-        for _row in data:
-            self.distribution.update(TextMetrics.inverted_cosine_similarity(centroid,_row))
-
+        self.distribution = [0]*data.shape[0]
+        for idx in range(data.shape[0]):
+            self.distribution[idx] = TextMetrics.inverted_cosine_similarity(centroid,data[idx])
+        self.distribution = CosineSimilarityDistribution.CosineSimilarityDistribution(self.nBins, self.distribution)
+        
         # Now that distribution is set up, we need to obtain the Data characteristics, namely the concentration of points...
         #self.max_peak_key = self.distribution.dist.index(max(self.distribution.dist))
-        self.delta_low_index, self.delta_high_index, _ = self.getSubArray(self.distribution.dist, self.nBins, int(self.alpha*data.shape[0]))
+        # need from 1:-1 because the outside ones are "outliers"...
+        self.delta_low_index, self.delta_high_index, _ = array_utils.getSubArray(self.distribution.dist[1:-1], self.nBins-2, int(self.alpha*data.shape[0]))
+        self.delta_high_index+=1
+        self.delta_low_index+=1
         self.attrs["delta_low"] = self.distribution.dist_keys[self.delta_low_index] - (1./self.nBins)
         self.attrs["delta_high"] = self.distribution.dist_keys[self.delta_high_index]
-
-
-
-    # https://www.geeksforgeeks.org/subarray-whose-absolute-sum-is-closest-to-k/
-    # Improve with nlogn solution at https://www.geeksforgeeks.org/subarray-whose-sum-is-closest-to-k/
-    # TODO
-    def getSubArray(self,arr, n, K): 
-        currSum = 0
-        prevDif = 0
-        currDif = 0
-        result = [-1, -1, abs(K-abs(currSum))] 
-        resultTmp = result 
-        i = 0
-        j = 0
-        while(i<= j and j<n): 
-            currSum += arr[j] 
-            prevDif = currDif 
-            currDif = K - abs(currSum) 
-            if(currDif <= 0): 
-                if abs(currDif) < abs(prevDif): 
-                    resultTmp = [i, j, currDif] 
-                else: 
-                    resultTmp = [i, j-1, prevDif] 
-                currSum -= (arr[i]+arr[j])                 
-                i += 1
-            else: 
-                resultTmp = [i, j, currDif] 
-                j += 1                
-            if(abs(resultTmp[2]) < abs(result[2])): 
-                result = resultTmp 
-        return result 
+ 
     
     def get(self,_key):
         return self.attrs[_key]
--- mlep/mlep/text/DataCharacteristics/L2NormDataCharacteristics.py
+++ mlep/mlep/text/DataCharacteristics/L2NormDataCharacteristics.py
@@ -1,18 +1,19 @@
 import numpy as np
+import mlep.tools.distributions.DistanceDistribution as DistanceDistribution
+import mlep.utils.array_utils as array_utils
 
 class L2NormDataCharacteristics:
     def __init__(self, nBins=40, alpha = 0.6):
         self.nBins = nBins
-        self.distribution = []
+        self.distribution = None
         self.alpha = alpha
+        
         self.attrs={}
         self.attrs["centroid"] = None
         self.attrs["delta_low"] = None
         self.attrs["delta_high"] = None
         
-        self.dist_keys = [item*(1./self.nBins) for item in range(self.nBins+1)]
-        self.dist = None
-
+        
     def buildDistribution(self,centroid,data):
         """ Build the data distribution given a data set and its centroid.
 
@@ -21,54 +22,19 @@ class L2NormDataCharacteristics:
         """
 
         self.attrs["centroid"] = centroid
+
+        
         self.distribution = [0]*data.shape[0]
-        n_min = float("inf")
-        n_max = -float("inf")
         for idx in range(data.shape[0]):
             self.distribution[idx] = np.linalg.norm(data[idx]-centroid)
-            n_max = self.distribution[idx] if self.distribution[idx] > n_max else n_max
-            n_min = self.distribution[idx] if self.distribution[idx] < n_min else n_min
-        n_range = n_max - n_min
-        self.dist = [np.where(np.logical_and(
-                            np.less_equal(self.distribution,n_min + (n_range*self.dist_keys[didx])),
-                            np.greater_equal(self.distribution,n_min + (n_range*self.dist_keys[didx-1]))))[0].shape[0] for didx in range(self.nBins+1)]
+        self.distribution = DistanceDistribution.DistanceDistribution(self.nBins, self.distribution)
 
-        # Now that distribution is set up, we need to obtain the Data characteristics, namely the concentration of points...
-        #self.max_peak_key = self.distribution.dist.index(max(self.distribution.dist))
-        self.delta_low_index, self.delta_high_index, _ = self.getSubArray(self.dist, self.nBins, int(self.alpha*data.shape[0]))
-        self.attrs["delta_low"] = self.dist_keys[self.delta_low_index] - (1./self.nBins)
-        self.attrs["delta_high"] = self.dist_keys[self.delta_high_index]
+        self.delta_low_index, self.delta_high_index, _ = array_utils.getSubArray(self.distribution.dist[1:-1], self.nBins-2, int(self.alpha*data.shape[0]))
+        self.delta_high_index+=1
+        self.delta_low_index+=1
+        self.attrs["delta_low"] = self.distribution.dist_keys[self.delta_low_index] - (1./self.nBins)
+        self.attrs["delta_high"] = self.distribution.dist_keys[self.delta_high_index]
 
-
-
-    # https://www.geeksforgeeks.org/subarray-whose-absolute-sum-is-closest-to-k/
-    # Improve with nlogn solution at https://www.geeksforgeeks.org/subarray-whose-sum-is-closest-to-k/
-    # TODO
-    def getSubArray(self,arr, n, K): 
-        currSum = 0
-        prevDif = 0
-        currDif = 0
-        result = [-1, -1, abs(K-abs(currSum))] 
-        resultTmp = result 
-        i = 0
-        j = 0
-        while(i<= j and j<n): 
-            currSum += arr[j] 
-            prevDif = currDif 
-            currDif = K - abs(currSum) 
-            if(currDif <= 0): 
-                if abs(currDif) < abs(prevDif): 
-                    resultTmp = [i, j, currDif] 
-                else: 
-                    resultTmp = [i, j-1, prevDif] 
-                currSum -= (arr[i]+arr[j])                 
-                i += 1
-            else: 
-                resultTmp = [i, j, currDif] 
-                j += 1                
-            if(abs(resultTmp[2]) < abs(result[2])): 
-                result = resultTmp 
-        return result 
 
     def get(self,_key):
         return self.attrs[_key]
